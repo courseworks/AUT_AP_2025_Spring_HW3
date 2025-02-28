d81cd7a4e69afab712d8855e1813d50fd0d15173
smhamidi <1380hamidi@gmail.com>
2025-02-28 16:31:07 +0330

    add initial files

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+#include "InsuranceCompany.h"
+#include "InsurancePolicy.h"
+#include "Person.h"
+
+int main(int argc, char **argv) {
+	if (true)  // Set to false to run unit-tests
+	{
+		// Debug section: Place your debugging code here
+	} else {
+		::testing::InitGoogleTest(&argc, argv);
+		std::cout << "RUNNING TESTS ..." << std::endl;
+		int ret{RUN_ALL_TESTS()};
+		if (!ret)
+			std::cout << "<<<SUCCESS>>>" << std::endl;
+		else
+			std::cout << "FAILED" << std::endl;
+	}
+	return 0;
+}
